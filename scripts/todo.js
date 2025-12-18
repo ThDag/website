ae5103500a264b14344181ed8d8c9d135a98eb65
taha <tahadoktor@gmail.com>
2025-12-18 20:43:34 +0300

    added new task system but its useless right now (the new task thing(and the rest of course))

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,12 +1,26 @@
 const body = document.getElementById("body");
+console.log(body);
 
 const newTaskButton = document.getElementById("newTaskButton");
+console.log(todos);
 
-console.log(body);
+
+
 
 newTaskButton.addEventListener("click", () => {
 
+  let todoText = "this is a test";
   console.log("working")
+
+  let newTaskTemplate = `
+      <div class="todo" id="todo"><span class="todoCheckBox"></span><span class="todoText">${todoText}</span></div>
+`
+
+
+  let todos = document.getElementById("todos").innerHTML;
+  document.getElementById("todos").innerHTML = `${todos} ${newTaskTemplate}`
+
+
 
 })
 
