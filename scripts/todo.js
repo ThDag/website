c269c59a050970a89b08f1028f1dbc431593bbac
taha <tahadoktor@gmail.com>
2025-12-18 17:16:11 +0300

    todo website initial commit

---
+++ scripts/todo.js
@@ -0,0 +1,12 @@
+const body = document.getElementById("body");
+
+const newTaskButton = document.getElementById("newTaskButton");
+
+console.log(body);
+
+newTaskButton.addEventListener("click", () => {
+
+  console.log("working")
+
+})
+
