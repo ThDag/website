dabb6c70f5026a3882325d2e440fac991785b8fe
taha <tahadoktor@gmail.com>
2025-12-18 21:37:35 +0300

    todo app; adding and deleting works (without editting tho)

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,26 +1,35 @@
-const body = document.getElementById("body");
-console.log(body);
 
 const newTaskButton = document.getElementById("newTaskButton");
-console.log(todos);
+const deleteTaskButton = document.getElementById("deleteTaskButton");
+const todos = document.getElementById('todos')
 
+// console.log(document.getElementById("todos").children);
+// let jfds = document.getElementsByClassName('todo')
+// console.log(jfds)
 
-
+let newestTodoId = 0;
 
 newTaskButton.addEventListener("click", () => {
-
   let todoText = "this is a test";
-  console.log("working")
 
+  newestTodoId += 1;
   let newTaskTemplate = `
-      <div class="todo" id="todo"><span class="todoCheckBox"></span><span class="todoText">${todoText}</span></div>
-`
-
+      <div class="todo" data-todoid="${newestTodoId}"><span class="todoCheckBox"></span><span class="todoText">${todoText} ${newestTodoId}</span></div>`
 
   let todos = document.getElementById("todos").innerHTML;
   document.getElementById("todos").innerHTML = `${todos} ${newTaskTemplate}`
 
 
-
 })
 
+deleteTaskButton.addEventListener("click", () => {
+  document.querySelector(`[data-todoid="${newestTodoId}"]`).remove();
+  newestTodoId -= 1;
+})
+
+todos.addEventListener("click", (event) => {
+  if (event.target.className === "todoCheckBox") {
+    console.log(event.target.parentElement.dataset.todoid)
+    event.target.parentElement.remove()
+  }
+})
