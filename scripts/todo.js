4f51266f2b8d1aba71f1d9a0a471c5805d801ed4
taha <tahadoktor@gmail.com>
2025-12-27 08:23:46 +0300

    fixed error; new todo deleting olds.
    when creating a new element it was rewriting the old ones which created
    two problems; 1- all the text in the old elements disappeared. 2- todos
    array pointed to the old dead ones.
    
    now its all fixed; yipeee

--- scripts/todo.js
+++ scripts/todo.js
@@ -28,8 +28,8 @@ newTaskButton.addEventListener("click", () => {
   let newTaskTemplate = `
   <div class="todo" data-todoid="${newestTodoId}"><span class="todoCheckBox"></span><input class="todoInputTag" type="text" placeholder="to-do"></div>`
 
-  let todos = document.getElementById("todos").innerHTML;
-  document.getElementById("todos").innerHTML = `${todos} ${newTaskTemplate}`
+  // let todos = document.getElementById("todos").innerHTML;
+  document.getElementById("todos").insertAdjacentHTML("beforeend", newTaskTemplate)
 
   newObj = new todoObject(newestTodoId);
   inventory.push(newObj);
@@ -45,27 +45,26 @@ todos.addEventListener("click", (event) => {
 
     let todoId = event.target.parentElement.dataset.todoid;
 
-    let completedTodosHtml = document.getElementById("completedTodos").innerHTML;
-    console.log(completedTodosHtml)
-    let todoHtml = inventory.find(item => String(item.id) === String(todoId)).htmlElement.parentElement.outerHTML;
-    console.log(todoHtml)
-    let todoText = document.querySelector(`[data-todoid="${event.target.parentElement.dataset.todoid}"] .todoInputTag`).value;
-    // let odoText = inventory.find(item => String(item.id) === String(todoId)).htmlElement.value;
-    // console.log(odoText)
+    // this code down there doesn't work for some reason; it doesn't get the string instead gives <empty string>
+    let todoText = inventory.find(item => String(item.id) === String(todoId)).htmlElement.value
+    console.log(todoText)
+
+    let todoHtml = inventory.find(item => String(item.id) === String(todoId)).htmlElement.parentElement;
+    // let todoText = document.querySelector(`[data-todoid="${event.target.parentElement.dataset.todoid}"] .todoInputTag`).value;
 
 
     event.target.parentElement.remove()
 
     // adds todo to the html and updates the value(text inside) to be what it was.
-    document.getElementById("completedTodos").innerHTML = `${completedTodosHtml}${todoHtml}`;
-    document.querySelector(`[data-todoid="${event.target.parentElement.dataset.todoid}"] .todoInputTag`).value = todoText;
+    document.getElementById("completedTodos").prepend(todoHtml);
+    document.querySelector(`[data-todoid="${todoId}"] .todoInputTag`).value = todoText;
   }
 })
 
 
 testButton.addEventListener("click", () => {
   const bruhmoment = inventory.find(item => item.id === 2);
-  console.log(bruhmoment.htmlElement);
+  console.log(bruhmoment.htmlElement.value);
   // console.log(bruhmoment.htmlElement.value);
   // console.log(document.querySelector('[data-todoid="2"] .todoInputTag').value);
 })
