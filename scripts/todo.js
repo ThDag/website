75949eaa15abf1fe0f9d69f73b4ef724781a3c16
taha <tahadoktor@gmail.com>
2025-12-28 17:38:44 +0300

    primitive checkbox tick design.
    added a tick to the checkbox with svg and path. its polished enough as
    it is but will change in the future.

--- scripts/todo.js
+++ scripts/todo.js
@@ -22,7 +22,7 @@ newTaskButton.addEventListener("click", () => {
   newestTodoId += 1;
 
   let newTaskTemplate = `
-  <div class="todo" data-todoid="${newestTodoId}"><span class="todoCheckBox"></span><input class="todoInputTag" type="text" placeholder="to-do"></div>`
+  <div class="todo" data-todoid="${newestTodoId}"><div class="todoCheckBox"><svg class="checkBoxTickContainer invisible" height="32" width="32" viewBox="0 0 100 100"><path class="linePath checkBoxTickPath" d="M 10 50 Q 30 60, 40 90 Q 50 50, 90 10" /></svg></div><input class="todoInputTag" type="text" placeholder="to-do"></div>`
 
   // let todos = document.getElementById("todos").innerHTML;
   document.getElementById("todos").insertAdjacentHTML("beforeend", newTaskTemplate)
@@ -35,26 +35,31 @@ newTaskButton.addEventListener("click", () => {
 
 todos.addEventListener("click", (event) => {
 
+  // console.log(event.target.classList)
+
   // complete task
   if (event.target.className === "todoCheckBox") {
-    console.log(event.target.parentElement.dataset.todoid)
+    console.log("completed todo id;", event.target.parentElement.dataset.todoid)
 
     let todoId = event.target.parentElement.dataset.todoid;
     itemFromInventory = inventory.find(item => String(item.id) === String(todoId));
 
     // this code down there doesn't work for some reason; it doesn't get the string instead gives <empty string>
     let todoText = itemFromInventory.htmlElement.value
-    console.log(todoText)
 
-    let todoHtml = itemFromInventory.htmlElement.parentElement;
+    let todoHtmlElement = itemFromInventory.htmlElement.parentElement;
     // let todoText = document.querySelector(`[data-todoid="${event.target.parentElement.dataset.todoid}"] .todoInputTag`).value;
 
 
     event.target.parentElement.remove()
 
     // adds todo to the html and updates the value(text inside) to be what it was.
-    document.getElementById("completedTodos").prepend(todoHtml);
+    document.getElementById("completedTodos").prepend(todoHtmlElement);
     document.querySelector(`[data-todoid="${todoId}"] .todoInputTag`).value = todoText;
+
+    itemFromInventory.htmlElement.parentElement.querySelector(".checkBoxTickContainer").classList.remove("invisible")
+    itemFromInventory.htmlElement.parentElement.querySelector(".checkBoxTickContainer").classList.add("visible")
+    console.log(itemFromInventory.htmlElement.parentElement.querySelector(".checkBoxTickContainer"))
   }
 })
 
