f298c5eb0abe1a1be1ced39d1110fb44a88801a9
taha <tahadoktor@gmail.com>
2025-12-30 23:08:23 +0300

    styling, cleaning code, transition testing

--- scripts/todo.js
+++ scripts/todo.js
@@ -8,7 +8,7 @@ class todoObject {
     // it's not possible to have a value when the to-do is first created
     // this.value = undefined;
     this.id = id;
-    this.htmlElement = document.querySelector(`[data-todoid="${id}"] .todoInputTag`)
+    this.htmlElement = document.querySelector(`[data-todoid="${id}"]`)
     this.isCompleted = false;
   }
 }
@@ -22,9 +22,9 @@ newTaskButton.addEventListener("click", () => {
   newestTodoId += 1;
 
   let newTaskTemplate = `
-  <div class="todo" data-todoid="${newestTodoId}"><div class="todoCheckBox"><svg class="checkBoxTickContainer invisible" height="32" width="32" viewBox="0 0 100 100"><path class="linePath checkBoxTickPath" d="M 10 50 Q 30 60, 40 90 Q 50 50, 90 10" /></svg></div><input class="todoInputTag" type="text" placeholder="to-do"></div>`
+  <div class="todo" data-todoid="${newestTodoId}"><div class="todoCheckBox"><svg class="checkBoxTickContainer" height="32" width="32" viewBox="0 0 100 100"><path class="linePath checkBoxTickPath" d="M 10 50 Q 30 60, 40 90 Q 50 50, 90 10" /></svg></div><input class="todoInputTag" type="text" placeholder="to-do"></div>`
 
-  document.getElementById("activeTodos").insertAdjacentHTML("afterbegin", newTaskTemplate)
+  document.getElementById("todos").insertAdjacentHTML("afterbegin", newTaskTemplate)
 
   newObj = new todoObject(newestTodoId);
   inventory.push(newObj);
@@ -32,7 +32,7 @@ newTaskButton.addEventListener("click", () => {
 })
 
 
-todos = document.querySelector("#todos")
+todos = document.querySelector("#todosContainer")
 todos.addEventListener("click", (event) => {
 
   // console.log(event.target.classList)
@@ -42,24 +42,24 @@ todos.addEventListener("click", (event) => {
     console.log("completed todo id;", event.target.parentElement.dataset.todoid)
 
     let todoId = event.target.parentElement.dataset.todoid;
-    itemFromInventory = inventory.find(item => String(item.id) === String(todoId));
+    let itemFromInventory = inventory.find(item => String(item.id) === String(todoId));
 
-    // this code down there doesn't work for some reason; it doesn't get the string instead gives <empty string>
-    let todoText = itemFromInventory.htmlElement.value
 
-    let todoHtmlElement = itemFromInventory.htmlElement.parentElement;
+    itemFromInventory.htmlElement.querySelector(".checkBoxTickContainer").classList.add("transitionTest")
+
+    let todoText = itemFromInventory.htmlElement.querySelector(".todoInputTag").value
+
+    let todoHtmlElement = itemFromInventory.htmlElement;
     // let todoText = document.querySelector(`[data-todoid="${event.target.parentElement.dataset.todoid}"] .todoInputTag`).value;
-
 
     event.target.parentElement.remove()
 
     // adds todo to the html and updates the value(text inside) to be what it was.
     document.getElementById("completedTodos").prepend(todoHtmlElement);
-    document.querySelector(`[data-todoid="${todoId}"] .todoInputTag`).value = todoText;
+    itemFromInventory.htmlElement.querySelector(".todoInputTag").value = todoText;
 
-    itemFromInventory.htmlElement.parentElement.querySelector(".checkBoxTickContainer").classList.remove("invisible")
-    itemFromInventory.htmlElement.parentElement.querySelector(".checkBoxTickContainer").classList.add("visible")
-    console.log(itemFromInventory.htmlElement.parentElement.querySelector(".checkBoxTickContainer"))
+    itemFromInventory.htmlElement.querySelector(".checkBoxTickContainer").classList.remove("invisible")
+    console.log(itemFromInventory.htmlElement.querySelector(".checkBoxTickContainer"))
   }
 })
 
