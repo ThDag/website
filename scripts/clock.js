c1ec9a8ac06f6907f4908ec8dd2a8cc4bfd52f66
taha <tahadoktor@gmail.com>
2026-01-09 15:33:42 +0300

    clock thing finished

--- scripts/clock.js
+++ scripts/clock.js
@@ -18,14 +18,9 @@ function secondHandFunction() {
   let minutesAngle = `${(date.getMinutes() * 6) + 90}deg`
   minuteHandElement.style.rotate = minutesAngle
 
-  let hoursAngle = `${(date.getHours() * 6) + 90}deg`
   let hour = date.getHours();
-  if (hour >= 12) {
-    hour = hour - 12;
-  }
-
-  hoursAngle = `${hour * 30 + 90}deg`;
-
+  if (hour >= 12) { hour -= 12; };
+  let hoursAngle = `${hour * 30 + 90}deg`;
   hourHandElement.style.rotate = hoursAngle;
 
 
