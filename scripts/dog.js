b301f9fc45c75b01e3eac8ecd8a9ed2460ffd57f
taha <tahadoktor@gmail.com>
2026-01-26 12:46:00 +0300

    dog breed selection with the selected breeds showing in pill badges; working

--- scripts/dog.js
+++ scripts/dog.js
@@ -1,12 +1,11 @@
 
 const mainImage = document.getElementById("mainImage");
 const dogBreeds = document.querySelector(".dogBreeds");
-
+const selectedBreedsBadges = document.querySelector(".selectedBreedsBadges");
 
 const randomDogUrl = "https://dog.ceo/api/breeds/image/random";
 const breedListUrl = "https://dog.ceo/api/breeds/list/all";
 
-const breedListArray = [];
 
 fetch(randomDogUrl)
   .then(response => response.json())
@@ -17,10 +16,17 @@ fetch(randomDogUrl)
   })
   .catch(error => console.log("sike sorry", error))
 
-
 function htmlizeArray(array) {
-  console.log(array)
+  // takes a list of ids, returns a html string of dog breed names in pill shaped badges
+  let htmlizedArray = array.map((item) => {
+    return `<span class="badge fw-medium rounded-pill text-primary border border-2 border-primary">${breedListArray[Number(item)]}</span>`
+  })
+
+  return htmlizedArray.join(" ")
+
 }
+
+const breedListArray = [];
 
 fetch(breedListUrl)
   .then(response => response.json())
@@ -29,3 +35,28 @@ fetch(breedListUrl)
   .catch(error => console.log("errorr", error))
 
 
+// list of selected breeds in index form from the breedlistArray
+let selectedBreeds = [];
+
+document.addEventListener("click", (event) => {
+
+
+  if (event.target.matches(".breedListItem")) {
+    if (!event.target.matches(".active")) {
+      event.target.classList.toggle("active")
+      selectedBreeds.push(event.target.dataset.breedid)
+
+
+    }
+    else if (event.target.matches(".active")) {
+      event.target.classList.toggle("active")
+
+      // this is the only way to remove something from an array with the value
+      selectedBreeds = selectedBreeds.filter(item => item !== event.target.dataset.breedid)
+    }
+
+    selectedBreedsBadges.innerHTML = htmlizeArray(selectedBreeds)
+  }
+  // console.log(selectedBreeds)
+
+})
