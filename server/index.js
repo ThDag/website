f8ab9a49c48e2e0afc3c0f5aee47f35b59067a0b
taha <tahadoktor@gmail.com>
2026-02-24 17:14:50 +0300

    barely started to learn backend

--- server/index.js
+++ server/index.js
@@ -1,5 +1,21 @@
-import http from "http"
+const http = require('http');
+const PORT = 3000
 
+
+// Create a server object
 const server = http.createServer((req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-})
+  // Send the response body as 'Hello, World!'
+  console.log(req.headers.cookie)
+  res.write("first response \n")
+  res.end('second and last response Hello, World!\n');
+});
+
+// Define the port to listen on const PORT = 3000;
+
+// Start the server and listen on the specified port
+server.listen(PORT, 'localhost', () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
