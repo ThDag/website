6b159c10198b9602f788f3d209d735dd264dc3d9
taha <tahadoktor@gmail.com>
2026-02-28 17:14:16 +0300

    designing frontend to send post message to backend

--- client/scripts/form.js
+++ client/scripts/form.js
@@ -1,12 +1,36 @@
 submitButton = document.getElementById("mainSubmitButton")
+nameInput = document.getElementById("name")
+messageInput = document.getElementById("theMessage")
+
+
+async function sendMessageToBackend(body) {
+  try {
+    const response = await fetch("http://localhost:3000", {
+      method: "POST",
+      body: body
+    })
+
+    const result = await response.json()
+    console.log(result)
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
 
 document.addEventListener("click", (event) => {
 
   if (event.target === submitButton) {
     console.log("submit clicked")
+
+    let bodyData = {
+      name: nameInput.value,
+      message: messageInput.value
+    }
+
+    sendMessageToBackend(bodyData)
   }
 
-  else {
-  }
+  else { }
 
 })
