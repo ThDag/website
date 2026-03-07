528fff8b38f12322696bda99c4cc510d09313d27
taha <tahadoktor@gmail.com>
2026-03-07 17:15:34 +0300

    changed to 127 from localhost

--- server/index.js
+++ server/index.js
@@ -31,6 +31,6 @@ const server = http.createServer((req, res) => {
 // Define the port to listen on const PORT = 3000;
 
 // Start the server and listen on the specified port
-server.listen(PORT, 'localhost', () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}/`);
 });
