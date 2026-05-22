17d46a9a30d4b87bf514af9220e289c13239f902
taha <tahadoktor@gmail.com>
2026-05-22 15:57:34 +0300

    postgres finally working

--- server/personalBin.js
+++ server/personalBin.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 })
 
 const res = await pool.query("CREATE TABLE IF NOT EXISTS test_table (id SERIAL PRIMARY KEY, name TEXT, age INTEGER)")
-const res1 = await pool.query("SELECT * FROM postgres")
+const res1 = await pool.query("SELECT * FROM test_table")
 // const res = await pool.query("INSERT INTO test_table (name, age) VALUES ('nig', 6)")
 console.log(res1)
 
